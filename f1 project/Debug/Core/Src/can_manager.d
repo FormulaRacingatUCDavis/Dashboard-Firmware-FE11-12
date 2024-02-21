ef8b04ecd65cf3124e044a6fb79ae6eed68ecfb0
MelodyLiu012 <57297187+MelodyLiu012@users.noreply.github.com>
2024-02-20 18:12:16 -0800

    restarted project

--- f1 project/Debug/Core/Src/can_manager.d
+++
@@ -1,7 +0,0 @@
-Core/Src/can_manager.o: ../Core/Src/can_manager.c \
- ../Core/Src/can_manager.h ../Core/Src/fsm.h ../Core/Src/sensors.h \
- ../Core/Src/config.h
-../Core/Src/can_manager.h:
-../Core/Src/fsm.h:
-../Core/Src/sensors.h:
-../Core/Src/config.h:
--- f1 project/Debug/Core/Src/sensors.d
+++
@@ -1,5 +0,0 @@
-Core/Src/sensors.o: ../Core/Src/sensors.c ../Core/Src/sensors.h \
- ../Core/Src/config.h ../Core/Src/fsm.h
-../Core/Src/sensors.h:
-../Core/Src/config.h:
-../Core/Src/fsm.h:
