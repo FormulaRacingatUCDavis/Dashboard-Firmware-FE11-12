4ac94f63021f5f67d304b0fc3555bbbdb035d7bf
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2024-05-17 23:28:49 -0700

    Made four-bit mode work and added .gitignore to ignore build and setting files

--- Dashboard_Firmware_Attempt2/Core/Src/main.c
+++ Dashboard_Firmware_Attempt2/Core/Src/main.c
@@ -551,11 +551,14 @@ static void MX_SDMMC1_SD_Init(void)
   hsd1.Init.ClockEdge = SDMMC_CLOCK_EDGE_RISING;
   hsd1.Init.ClockBypass = SDMMC_CLOCK_BYPASS_DISABLE;
   hsd1.Init.ClockPowerSave = SDMMC_CLOCK_POWER_SAVE_DISABLE;
-  hsd1.Init.BusWide = SDMMC_BUS_WIDE_1B;
+  hsd1.Init.BusWide = SDMMC_BUS_WIDE_4B;
   hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
   hsd1.Init.ClockDiv = 0;
   /* USER CODE BEGIN SDMMC1_Init 2 */
+  hsd1.Init.BusWide = SDMMC_BUS_WIDE_1B;
 
+  if (HAL_SD_Init(&hsd1) != HAL_OK) return;
+  if (HAL_SD_ConfigWideBusOperation(&hsd1, SDMMC_BUS_WIDE_4B) != HAL_OK) return;
   /* USER CODE END SDMMC1_Init 2 */
 
 }
