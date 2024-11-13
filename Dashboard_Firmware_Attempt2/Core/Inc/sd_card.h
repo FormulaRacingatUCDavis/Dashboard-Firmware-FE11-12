1e4e03aac15c82fcf96ed1b976f5b4559fe73bbf
Ian Miranda <ianmiranda@campus-001-109.ucdavis.edu>
2024-11-13 13:44:13 -0800

    Add SD Card binary writing

--- Dashboard_Firmware_Attempt2/Core/Inc/sd_card.h
+++ Dashboard_Firmware_Attempt2/Core/Inc/sd_card.h
@@ -13,8 +13,8 @@ SD_CARD_MOUNT_RESULT sd_card_mount(void);
 void sd_card_write_data_record(uint32_t id, uint8_t data[]);
 void sd_card_write_from_rx(CAN_RxHeaderTypeDef rxHeader, uint8_t rxData[]);
 void sd_card_write_from_tx(CAN_TxHeaderTypeDef txHeader, uint8_t txData[]);
-void sd_card_write_sync(void);
-void sd_card_write_async(void);
+void sd_card_update_sync(void);
+void sd_card_update_async(void);
 void sd_card_flush(void);
 
 #endif
